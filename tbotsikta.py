5dd71c9d411818a8777597bbcc4656f9e88eaf6b
Aman Agrawal <sprshag@gmail.com>
2020-05-22 18:24:48 +0530

    failed adding a feature.. commented it out

--- tbotsikta.py
+++ tbotsikta.py
@@ -15,41 +15,29 @@ def bop(update,context):
 	chat_id = update.message.chat_id
 	context.bot.send_photo(chat_id=chat_id, photo=url)
 
-# def Tbot_sendtext(bot_msg):
-
-# 	bot_token = '1194260976:AAGqYFgCJeDNzKX_vqlTIgl9gfMf9VMwLYU'
-# 	chat_ID = update.message.chat_id
-# 	send_text = 'https://api.telegram.org/bot'+bot_token+'/sendMessage?chat_id='+chat_ID+'&parse_mode=Markdown&text='+bot_msg
-
-# 	response = requests.get(send_text)
-
-# 	return response.json()
-
-
 def drinkwater(update,context):
 	msg = 'Well done, darling! I am proud of you! Keep going!'
 	chat_id = update.message.chat_id
 	context.bot.send_message(chat_id=chat_id,text=msg)
 
 def drinkrem(context):
-	msg='Hey baby! Its been 30 minutes, drink some water.'
+	msg='Hey baby! Its been {} minute(s), drink some water.'.format(timer/60)
 	context.bot.send_message(chat_id=context.job.context,text=msg)
 
 def startreminder(update,context):
-	context.bot.send_message(chat_id=update.message.chat_id,text='Starting the reminder to run every 30 minutes.')
-	context.job_queue.run_repeating(drinkrem,1800,context=update.message.chat_id,first=0)
+	context.bot.send_message(chat_id=update.message.chat_id,text='Starting the reminder to run every {} minute(s).'.format(timer/60))
+	context.job_queue.run_repeating(drinkrem,interval=timer,context=update.message.chat_id)
 
+# def settimer(update,context):
+# 	global timer
+# 	timin = ''.join(context.args)
+# 	timer = int(timin)*60
+# 	context.bot.send_message(chat_id=update.message.chat_id,text="You'll get reminder every {} minute(s) now.".format(timin))
 
 def bae(update,context):
 	msg = "I'm missing you! <3 Text me : @sprshag"
 	chat_id = update.message.chat_id
 	context.bot.send_message(chat_id=chat_id,text=msg)
-
-# schedule.every(30).minutes.do(drinkwater)
-
-# while True:
-# 	schedule.run_pending()
-# 	time.sleep(1)
 
 def shutdown():
 	updater.stop()
@@ -67,7 +55,7 @@ def start(update,context):
 	context.bot.send_message(chat_id=chat_id,text=msg)
 
 def helpe(update,context):
-	msg = "Hey sweetheart. Here's a list of commands for you:\n/start : to check if the bot is running.\n/reminder: to start/reset the reminder for something important.\n/woof : to get a surprise.\n/drank : if you drank a lot of water already.\n/bae : to talk to your bae.\n/help : to see the commands list.\n/stop : to stop the bot.\n"
+	msg = "Hey sweetheart. Here's a list of commands for you:\n/start : to check if the bot is running.\n/reminder : to start/reset the reminder for something important.\n/woof : to get a surprise.\n/drank : if you drank a lot of water already.\n/bae : to talk to your bae.\n/help : to see the commands list.\n/stop : to stop the bot.\n"
 	chat_id = update.message.chat_id
 	context.bot.send_message(chat_id=chat_id,text=msg)
 
@@ -79,10 +67,12 @@ def main():
 	dp.add_handler(CommandHandler('start', start))
 	dp.add_handler(CommandHandler('help', helpe))
 	dp.add_handler(CommandHandler('bae',bae))
+	#dp.add_handler(CommandHandler('settimer',settimer))
 	dp.add_handler(CommandHandler('reminder',startreminder))
 	updater.start_polling()
 	updater.idle()
 	
+timer = 1800
 
 updater = Updater('1194260976:AAGqYFgCJeDNzKX_vqlTIgl9gfMf9VMwLYU',use_context=True)
 
