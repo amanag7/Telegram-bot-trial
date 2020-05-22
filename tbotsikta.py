b4b128816f892a1606818e28e49803a729b6575e
Aman Agrawal <sprshag@gmail.com>
2020-05-22 12:03:39 +0530

    testing the commands

--- tbotsikta.py
+++ tbotsikta.py
@@ -14,20 +14,21 @@ def bop(bot,update):
 	chat_id = update.message.chat_id
 	bot.send_photo(chat_id=chat_id, photo=url)
 
-def Tbot_sendtext(bot_msg):
+# def Tbot_sendtext(bot_msg):
 
-	bot_token = '1194260976:AAGqYFgCJeDNzKX_vqlTIgl9gfMf9VMwLYU'
-	chat_ID = update.message.chat_id
-	send_text = 'https://api.telegram.org/bot'+bot_token+'/sendMessage?chat_id='+chat_ID+'&parse_mode=Markdown&text='+bot_msg
+# 	bot_token = '1194260976:AAGqYFgCJeDNzKX_vqlTIgl9gfMf9VMwLYU'
+# 	chat_ID = update.message.chat_id
+# 	send_text = 'https://api.telegram.org/bot'+bot_token+'/sendMessage?chat_id='+chat_ID+'&parse_mode=Markdown&text='+bot_msg
 
-	response = requests.get(send_text)
+# 	response = requests.get(send_text)
 
-	return response.json()
+# 	return response.json()
 
 
-def drinkwater():
+def drinkwater(bot,update):
 	msg = 'Hey baby, it has been 30 minutes, drink some water.'
-	Tbot_sendtext(msg)
+	chat_id = update.message.chat_id
+	bot.send_message(chat_id=chat_id,text=msg)
 
 # schedule.every(30).minutes.do(drinkwater)
 
@@ -39,9 +40,10 @@ def main():
 	updater = Updater('1194260976:AAGqYFgCJeDNzKX_vqlTIgl9gfMf9VMwLYU')
 	dp = updater.dispatcher
 	dp.add_handler(CommandHandler('woof',bop))
+	dp.add_handler(CommandHandler('drink',drinkwater))
 	updater.start_polling()
 	updater.idle()
-	drinkwater()
+	#drinkwater()
 
 if __name__=='__main__':
 	main()
