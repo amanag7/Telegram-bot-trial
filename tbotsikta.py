ccd1c58173aaac29286d8522439fe39c3b67fd91
Aman Agrawal <sprshag@gmail.com>
2020-05-22 16:25:41 +0530

    Talk to bae, text fixes and stopping notif

--- tbotsikta.py
+++ tbotsikta.py
@@ -1,7 +1,7 @@
 import time
 import schedule
 import requests
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, InlineQueryHandler, MessageHandler
 import re
 import threading
 
@@ -29,6 +29,11 @@ def bop(bot,update):
 def drinkwater(bot,update):
 	msg = 'Hey baby, it has been 30 minutes, drink some water.'
 	chat_id = update.message.chat_id
+	bot.send_message(chat_id=chat_id,text=msg)	
+
+def bae(bot,update):
+	msg = "He's missing you! <3 Go text him : @sprshag"
+	chat_id = update.message.chat_id
 	bot.send_message(chat_id=chat_id,text=msg)
 
 # schedule.every(30).minutes.do(drinkwater)
@@ -42,15 +47,18 @@ def shutdown():
 	updater.is_idle=False
 
 def stop(bot,update):
+	chat_id = update.message.chat_id
+	bot.send_message(chat_id=chat_id,text="Hey! I've stopped successfully. Take care!")
+	bot.send_message(chat_id='935814583',text="Hey, the bot has been stopped!")
 	threading.Thread(target=shutdown).start()
 
 def start(bot,update):
-	msg = "Hey baby! I'm still awake for you. Type /help for more commands."
+	msg = "Hey baby! I'm still awake. Type /help for more commands."
 	chat_id = update.message.chat_id
 	bot.send_message(chat_id=chat_id,text=msg)
 
 def helpe(bot,update):
-	msg = "Hey sweetheart. Here's a list of commands for you:\n/start : to check if the bot is running.\n/woof : to get a surprise.\n/drink : to remind yourself to do something inportant.\n/help : to see the commands list.\n/stop : to stop the bot.\n"
+	msg = "Hey sweetheart. Here's a list of commands for you:\n/start : to check if the bot is running.\n/woof : to get a surprise.\n/drink : to remind yourself to do something important.\n/bae : to talk to your bae.\n/help : to see the commands list.\n/stop : to stop the bot.\n"
 	chat_id = update.message.chat_id
 	bot.send_message(chat_id=chat_id,text=msg)
 
@@ -61,9 +69,10 @@ def main():
 	dp.add_handler(CommandHandler('stop', stop))
 	dp.add_handler(CommandHandler('start', start))
 	dp.add_handler(CommandHandler('help', helpe))
+	dp.add_handler(CommandHandler('bae',bae))
 	updater.start_polling()
 	updater.idle()
-	#drinkwater()
+	
 
 updater = Updater('1194260976:AAGqYFgCJeDNzKX_vqlTIgl9gfMf9VMwLYU')
 
