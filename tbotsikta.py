c860f15b65def461d690e5480d8dbd2b94cacf39
Aman Agrawal <sprshag@gmail.com>
2020-05-22 17:21:28 +0530

    Updated acc to library v12.0

--- tbotsikta.py
+++ tbotsikta.py
@@ -1,8 +1,8 @@
 import time
 import schedule
 import requests
-from telegram.ext import Updater, CommandHandler, InlineQueryHandler, MessageHandler
-import re
+from telegram.ext import Updater, CommandHandler, InlineQueryHandler, MessageHandler, CallbackContext
+import telegram
 import threading
 
 def get_url():
@@ -10,10 +10,10 @@ def get_url():
 	url = contents['url']
 	return url
 
-def bop(bot,update):
+def bop(update,context):
 	url=get_url()
 	chat_id = update.message.chat_id
-	bot.send_photo(chat_id=chat_id, photo=url)
+	context.bot.send_photo(chat_id=chat_id, photo=url)
 
 # def Tbot_sendtext(bot_msg):
 
@@ -26,15 +26,18 @@ def bop(bot,update):
 # 	return response.json()
 
 
-def drinkwater(bot,update):
+def drinkwater(update,context):
 	msg = 'Hey baby, it has been 30 minutes, drink some water.'
 	chat_id = update.message.chat_id
-	bot.send_message(chat_id=chat_id,text=msg)	
+	context.bot.send_message(chat_id=chat_id,text=msg)
 
-def bae(bot,update):
+#def startreminder(update: telegram.Update,context: CallbackContext):
+
+
+def bae(update,context):
 	msg = "He's missing you! <3 Go text him : @sprshag"
 	chat_id = update.message.chat_id
-	bot.send_message(chat_id=chat_id,text=msg)
+	context.bot.send_message(chat_id=chat_id,text=msg)
 
 # schedule.every(30).minutes.do(drinkwater)
 
@@ -46,21 +49,21 @@ def shutdown():
 	updater.stop()
 	updater.is_idle=False
 
-def stop(bot,update):
+def stop(update,context):
 	chat_id = update.message.chat_id
-	bot.send_message(chat_id=chat_id,text="Hey! I've stopped successfully. Take care!")
-	bot.send_message(chat_id='935814583',text="Hey, the bot has been stopped!")
+	context.bot.send_message(chat_id=chat_id,text="Hey! I've stopped successfully. Take care!")
+	context.bot.send_message(chat_id='935814583',text="Hey, the bot has been stopped!")
 	threading.Thread(target=shutdown).start()
 
-def start(bot,update):
+def start(update,context):
 	msg = "Hey baby! I'm still awake. Type /help for more commands."
 	chat_id = update.message.chat_id
-	bot.send_message(chat_id=chat_id,text=msg)
+	context.bot.send_message(chat_id=chat_id,text=msg)
 
-def helpe(bot,update):
+def helpe(update,context):
 	msg = "Hey sweetheart. Here's a list of commands for you:\n/start : to check if the bot is running.\n/woof : to get a surprise.\n/drink : to remind yourself to do something important.\n/bae : to talk to your bae.\n/help : to see the commands list.\n/stop : to stop the bot.\n"
 	chat_id = update.message.chat_id
-	bot.send_message(chat_id=chat_id,text=msg)
+	context.bot.send_message(chat_id=chat_id,text=msg)
 
 def main():
 	dp = updater.dispatcher
@@ -74,7 +77,7 @@ def main():
 	updater.idle()
 	
 
-updater = Updater('1194260976:AAGqYFgCJeDNzKX_vqlTIgl9gfMf9VMwLYU')
+updater = Updater('1194260976:AAGqYFgCJeDNzKX_vqlTIgl9gfMf9VMwLYU',use_context=True)
 
 
 if __name__=='__main__':
